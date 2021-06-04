d3d26fc5fd70115219b65efdb47d3f73ac0c6d48
theroyalbob <a.stew1998@gmail.com>
2021-06-04 14:48:02 -0400

    Update gapminder to include accurate data

--- Homework 3 Question 1.R
+++ Homework 3 Question 1.R
@@ -1,5 +1,7 @@
 require(tidyverse)
 require(gapminder)
 
-gapminder1 <- gapminder
+gapminder1 <- gapminder%>%
+  filter(continent == "Americas" & year == 1997)%>%
+  arrange(desc(lifeExp))
 gapminder1
